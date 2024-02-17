1d28f07c6ef112dc26eea1a625fce520f698a6da
HJacksons <jacksonherberts@gmail.com>
2024-02-17 21:45:00 +0100

    predict

--- src/vggtrain.py
+++ src/vggtrain.py
@@ -20,67 +20,67 @@ wandb.init(project=os.getenv("WANDB_PROJECT"), entity=os.getenv("WANDB_ENTITY"))
 dataset = Dataset()
 train_loader, vali_loader, test_loader = dataset.prepare_dataset()
 
-# Model
-model = VGG()
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
-
-# Train the model, log train loss, train accuracy, validation loss, validation accuracy
+# # Model
+# model = VGG()
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+#
+# # Loss and optimizer
+# criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+#
+# # Train the model, log train loss, train accuracy, validation loss, validation accuracy
 epochs = 50
-
-for epoch in range(epochs):
-    model.train()
-    total_loss, total_correct, total_samples = 0, 0, 0
-
-    for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        total_correct += (predicted == labels).sum().item()
-        total_samples += labels.size(0)
-
-    avg_loss = total_loss / len(train_loader)
-    avg_accuracy = total_correct / total_samples
-    logging.info(
-        f"Epoch [{epoch+1}/{epochs}], Train Loss: {avg_loss:.4f}, Train Accuracy: {avg_accuracy:.4f}"
-    )
-    wandb.log({"Train Loss": avg_loss, "Train Accuracy": avg_accuracy})
-
-    model.eval()
-    total_loss, total_correct, total_samples = 0, 0, 0
-
-    with torch.no_grad():
-        for images, labels in vali_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-
-            total_loss += loss.item()
-            _, predicted = torch.max(outputs, 1)
-            total_correct += (predicted == labels).sum().item()
-            total_samples += labels.size(0)
-
-    avg_loss = total_loss / len(vali_loader)
-    avg_accuracy = total_correct / total_samples
-    logging.info(
-        f"Epoch [{epoch+1}/{epochs}], Validation Loss: {avg_loss:.4f}, Validation Accuracy: {avg_accuracy:.4f}"
-    )
-    wandb.log({"Validation Loss": avg_loss, "Validation Accuracy": avg_accuracy})
-
-# Save the model
-torch.save(model.state_dict(), f"vgg_model_{epochs}.pth")
-wandb.save(f"vgg_model_{epochs}.pth")
-logging.info("Model saved.")
+#
+# for epoch in range(epochs):
+#     model.train()
+#     total_loss, total_correct, total_samples = 0, 0, 0
+#
+#     for images, labels in train_loader:
+#         images, labels = images.to(device), labels.to(device)
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#
+#         total_loss += loss.item()
+#         _, predicted = torch.max(outputs, 1)
+#         total_correct += (predicted == labels).sum().item()
+#         total_samples += labels.size(0)
+#
+#     avg_loss = total_loss / len(train_loader)
+#     avg_accuracy = total_correct / total_samples
+#     logging.info(
+#         f"Epoch [{epoch+1}/{epochs}], Train Loss: {avg_loss:.4f}, Train Accuracy: {avg_accuracy:.4f}"
+#     )
+#     wandb.log({"Train Loss": avg_loss, "Train Accuracy": avg_accuracy})
+#
+#     model.eval()
+#     total_loss, total_correct, total_samples = 0, 0, 0
+#
+#     with torch.no_grad():
+#         for images, labels in vali_loader:
+#             images, labels = images.to(device), labels.to(device)
+#             outputs = model(images)
+#             loss = criterion(outputs, labels)
+#
+#             total_loss += loss.item()
+#             _, predicted = torch.max(outputs, 1)
+#             total_correct += (predicted == labels).sum().item()
+#             total_samples += labels.size(0)
+#
+#     avg_loss = total_loss / len(vali_loader)
+#     avg_accuracy = total_correct / total_samples
+#     logging.info(
+#         f"Epoch [{epoch+1}/{epochs}], Validation Loss: {avg_loss:.4f}, Validation Accuracy: {avg_accuracy:.4f}"
+#     )
+#     wandb.log({"Validation Loss": avg_loss, "Validation Accuracy": avg_accuracy})
+#
+# # Save the model
+# torch.save(model.state_dict(), f"vgg_model_{epochs}.pth")
+# wandb.save(f"vgg_model_{epochs}.pth")
+# logging.info("Model saved.")
 
 
 # predicting
@@ -104,5 +104,7 @@ with torch.no_grad():
     _, predicted = torch.max(outputs, 1)
 
 # Print the predicted class
-logging.info(f"Predicted class: {predicted.item()}", "Actual class: {labels[0].item()}")
+logging.info(f"Predicted class: {predicted.item()}, Actual class: {labels[0].item()}")
+
+# Save the model
 
