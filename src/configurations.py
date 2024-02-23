081d9e890c10a0784a01ea5b24ad63f4bf564d10
HJacksons <jacksonherberts@gmail.com>
2024-02-23 12:22:53 +0100

    test on plantvillage aug false

--- src/configurations.py
+++ src/configurations.py
@@ -21,7 +21,22 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 CRITERION = nn.CrossEntropyLoss()
 EPOCHS = 50
 lr = 0.0001
-TRAINING = True
+
+DATA = "../data/plantVillage"
+TEST_SIZE = 0.2
+VALI_SIZE = 0.5
+RANDOM_STATE = 42  # for reproducibility
+BATCH_SIZE = 64
+CLASSES = sorted(os.listdir(DATA))
+# print list of classes
+# for i, cls in enumerate(CLASSES):
+#     print(f"{i}: {cls}")
+
+TRAINING = False
+AUGMENT = False
+
+NEW_DATASET = False
+
 if TRAINING:
     MODELS = {
         "VGG19": VGG19().to(DEVICE),
@@ -44,37 +59,41 @@ else:  # Testing
         "ViT": ViT,
     }
 
-DATA = "../data/plantVillage"
-TEST_SIZE = 0.2
-VALI_SIZE = 0.5
-RANDOM_STATE = 42  # for reproducibility
-BATCH_SIZE = 64
-CLASSES = sorted(os.listdir(DATA))
-# print list of classes
-# for i, cls in enumerate(CLASSES):
-#     print(f"{i}: {cls}")
-
-AUGMENT = False
-if AUGMENT:
-    SAVED_MODELS = {
-        "VGG19": "VGG19_best_model_Aug_True_153455.pth",
-        "ResNet50": "ResNet50_best_model_Aug_True_153455.pth",
-        "MobileV2": "MobileV2_best_model_Aug_True_153455.pth",
-        "ViT": "ViT_best_model_Aug_True_153455.pth",
-    }
+if NEW_DATASET:
+    if AUGMENT:
+        SAVED_MODELS = {
+            "VGG19": "VGG19_best_model_Aug_True_153455.pth",
+            "ResNet50": "ResNet50_best_model_Aug_True_153455.pth",
+            "MobileV2": "MobileV2_best_model_Aug_True_153455.pth",
+            "ViT": "ViT_best_model_Aug_True_153455.pth",
+        }
+    else:
+        SAVED_MODELS = {
+            "VGG19": "VGG19_best_model_Aug_False_153348.pth",
+            "ResNet50": "ResNet50_best_model_Aug_False_153348.pth",
+            "MobileV2": "MobileV2_best_model_Aug_False_153348.pth",
+            "ViT": "ViT_best_model_Aug_False_153348.pth",
+        }
 else:
-    SAVED_MODELS = {
-        "VGG19": "VGG19_best_model_Aug_False_153348.pth",
-        "ResNet50": "ResNet50_best_model_Aug_False_153348.pth",
-        "MobileV2": "MobileV2_best_model_Aug_False_153348.pth",
-        "ViT": "ViT_best_model_Aug_False_153348.pth",
-    }
-
+    if AUGMENT:
+        SAVED_MODELS = {
+            "VGG19": "VGG19_best_plantds_Aug_True_000353.pth",
+            "ResNet50": "ResNet50_best_plantds_Aug_True_000353.pth",
+            "MobileV2": "MobileV2_best_plantds_Aug_True_000353.pth",
+            "ViT": "ViT_best_plantds_Aug_True_000353.pth",
+        }
+    else:
+        SAVED_MODELS = {
+            "VGG19": "VGG19_best_plantds_Aug_False_005109.pth",
+            "ResNet50": "ResNet50_best_plantds_Aug_False_005109.pth",
+            "MobileV2": "MobileV2_best_plantds_Aug_False_005109.pth",
+            "ViT": "ViT_best_plantds_Aug_False_005109.pth",
+        }
 
 wandb.login(key=os.getenv("WANDB_KEY"))
 wandb.init(
     project=os.getenv("WANDB_PROJECT"),
     entity=os.getenv("WANDB_ENTITY"),
-    name=f"{time}_plantD_train_Aug_{AUGMENT}_{EPOCHS}epochs_bsize_{BATCH_SIZE}",  # Train name
-    # name=f"{time}_plantD_test_models_Aug_{AUGMENT}_bsize_{BATCH_SIZE}",  # Test name
+    # name=f"{time}_plantD_train_Aug_{AUGMENT}_{EPOCHS}epochs_bsize_{BATCH_SIZE}",  # Train name
+    name=f"{time}_plantD_test_models_Aug_{AUGMENT}_bsize_{BATCH_SIZE}",  # Test name
 )
