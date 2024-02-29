13ebe6ed578f5c982e39f806bcf18003a0e8629f
HJacksons <jacksonherberts@gmail.com>
2024-02-29 10:22:12 +0100

    test effnet off - modelat 76

--- src/testa.py
+++ src/testa.py
@@ -1,9 +1,10 @@
 from test import Tester  # adjust the import statement if needed
 from models import EfficientNetV2B3  # adjust the import statement if needed
+from configurations import MODELS  # adjust the import statement if needed
 
 
 def main():
-    tester = Tester(None, None, None, None)  # create Tester instance with dummy args
+    tester = Tester(MODELS, None, None, None)  # create Tester instance with dummy args
     model = tester.load_model(
         EfficientNetV2B3, "EfficientNetV2B3_last_potatodata_Aug_False_082520.pth"
     )
