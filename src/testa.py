aa709eb7df0049c25a2170945d7b86f7328b0e7d
HJacksons <jacksonherberts@gmail.com>
2024-02-29 10:57:51 +0100

    test new false augument

--- src/testa.py
+++ src/testa.py
@@ -1,15 +1,4 @@
-from test import Tester  # adjust the import statement if needed
-from models import EfficientNetV2B3  # adjust the import statement if needed
-from configurations import MODELS  # adjust the import statement if needed
-
-
-def main():
-    tester = Tester(MODELS, None, None, None)  # create Tester instance with dummy args
-    model = tester.load_model(
-        EfficientNetV2B3, "EfficientNetV2B3_last_potatodata_Aug_False_082520.pth"
-    )
-    print("Model loaded successfully")
-
-
-if __name__ == "__main__":
-    main()
+import torch
+import timm
+print(torch.__version__)
+print(timm.__version__)
