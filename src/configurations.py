149e249b2725813af35bc56ada9659bed30f5535
HJacksons <jacksonherberts@gmail.com>
2024-03-02 15:38:49 +0100

    test plantv final vit_hybrid true

--- src/configurations.py
+++ src/configurations.py
@@ -35,7 +35,7 @@ CLASSES = sorted(os.listdir(DATA))
 #     print(f"{i}: {cls}")
 
 TRAINING = False
-AUGMENT = False
+AUGMENT = True
 DATATYPE = "plantVillage"  # plantVillage or potatodata
 
 NEW_DATASET = False  # for the purpose of testing
