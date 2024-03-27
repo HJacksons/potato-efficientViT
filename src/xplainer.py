7d0605108df0743fbf90df66cb28ed81c3d1f0a5
HJacksons <jacksonherberts@gmail.com>
2024-03-28 00:19:07 +0100

    Txplainer vit

--- src/xplainer.py
+++ src/xplainer.py
@@ -67,7 +67,7 @@ for i, (input_tensors, labels) in enumerate(test_loader):
     # Generate an attention map for each input tensor
     for j in range(input_tensors.size(0)):  # Iterate over the batch dimension
         input_tensor = input_tensors[j]  # Select one image from the batch
-        attention_map = model.get_attention_map(input_tensor.unsqueeze(0))  # Add the batch dimension back
+        attention_map = model.get_attention_map(input_tensor)  # Add the batch dimension back
 
         # Now you can do something with the attention map, like saving it to a file
         attention_map_tensor = torch.from_numpy(attention_map)[None, :, :]
