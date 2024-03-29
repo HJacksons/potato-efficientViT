5c413c8b4314479078aacf9c7a44f561d1098195
HJacksons <jacksonherberts@gmail.com>
2024-03-29 16:46:32 +0100

    Test effhyind potato false

--- src/configurations.py
+++ src/configurations.py
@@ -24,7 +24,7 @@ CRITERION = nn.CrossEntropyLoss()
 EPOCHS = 70
 lr = 0.0001
 
-DATA = "../data/potatodata"  # "../data/potatodata
+DATA = "../data/plantVillage"  # "../data/potatodata
 TEST_SIZE = 0.1
 VALI_SIZE = 0.1
 RANDOM_STATE = 42  # for reproducibility
@@ -33,7 +33,7 @@ CLASSES = sorted(os.listdir(DATA))
 
 TRAINING = False
 AUGMENT = False
-DATATYPE = "potatodata"  # plantVillage or potatodata
+DATATYPE = "plantVillage"  # plantVillage or potatodata
 
 NEW_DATASET = False  # for the purpose of testing
 
