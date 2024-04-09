ae6b437ac1961ed4f85cd871358dff6be44aa13c
HJacksons <jacksonherberts@gmail.com>
2024-04-09 13:58:45 +0200

    train paper models true

--- src/configurations.py
+++ src/configurations.py
@@ -57,10 +57,10 @@ if TRAINING:
 
         # "HybridModelv2b3": HybridModelv2b3().to(DEVICE),
 
-        # "MobileNetV3_large": MobileNetV3_large().to(DEVICE),
+        "MobileNetV3_large": MobileNetV3_large().to(DEVICE),
         "VGG16": VGG16().to(DEVICE),
-        # "ResNet50": ResNet50().to(DEVICE),
-        # "DenseNet121": DenseNet121().to(DEVICE),
+        "ResNet50": ResNet50().to(DEVICE),
+        "DenseNet121": DenseNet121().to(DEVICE),
     }
     # model = MODELS["HybridModel"]  # Your hybrid model instance
 
@@ -81,12 +81,12 @@ if TRAINING:
         #"HybridInceptionV3": optim.Adam(MODELS["HybridInceptionV3"].parameters(), lr, weight_decay=0.0001),
         # "HybridXception": optim.Adam(MODELS["HybridXception"].parameters(), lr, weight_decay=0.0001),
 
-        # "HybridModelv2b3": optim.Adam(MODELS["HybridModelv2b3"].parameters(), lr, weight_decay=0.0001),
+        #"HybridModelv2b3": optim.Adam(MODELS["HybridModelv2b3"].parameters(), lr, weight_decay=0.0001),
 
-        # "MobileNetV3_large": optim.Adam(MODELS["MobileNetV3_large"].parameters(), lr, weight_decay=0.0001),
+        "MobileNetV3_large": optim.Adam(MODELS["MobileNetV3_large"].parameters(), lr, weight_decay=0.0001),
         "VGG16": optim.Adam(MODELS["VGG16"].parameters(), lr, weight_decay=0.0001),
-        # "ResNet50": optim.Adam(MODELS["ResNet50"].parameters(), lr, weight_decay=0.0001),
-        # "DenseNet121": optim.Adam(MODELS["DenseNet121"].parameters(), lr, weight_decay=0.0001),
+        "ResNet50": optim.Adam(MODELS["ResNet50"].parameters(), lr, weight_decay=0.0001),
+        "DenseNet121": optim.Adam(MODELS["DenseNet121"].parameters(), lr, weight_decay=0.0001),
     }
     SCHEDULER = {
         # "EfficientNetV2B3": optim.lr_scheduler.ReduceLROnPlateau(
@@ -130,18 +130,18 @@ if TRAINING:
         #     OPTIMIZERS["HybridModelv2b3"], patience=5, factor=0.5, verbose=True
         # ),
 
-        # "MobileNetV3_large": optim.lr_scheduler.ReduceLROnPlateau(
-        #     OPTIMIZERS["MobileNetV3_large"], patience=5, factor=0.5, verbose=True
-        # ),
+        "MobileNetV3_large": optim.lr_scheduler.ReduceLROnPlateau(
+            OPTIMIZERS["MobileNetV3_large"], patience=5, factor=0.5, verbose=True
+        ),
         "VGG16": optim.lr_scheduler.ReduceLROnPlateau(
             OPTIMIZERS["VGG16"], patience=5, factor=0.5, verbose=True
         ),
-        # "ResNet50": optim.lr_scheduler.ReduceLROnPlateau(
-        #     OPTIMIZERS["ResNet50"], patience=5, factor=0.5, verbose=True
-        # ),
-        # "DenseNet121": optim.lr_scheduler.ReduceLROnPlateau(
-        #     OPTIMIZERS["DenseNet121"], patience=5, factor=0.5, verbose=True
-        # ),
+        "ResNet50": optim.lr_scheduler.ReduceLROnPlateau(
+            OPTIMIZERS["ResNet50"], patience=5, factor=0.5, verbose=True
+        ),
+        "DenseNet121": optim.lr_scheduler.ReduceLROnPlateau(
+            OPTIMIZERS["DenseNet121"], patience=5, factor=0.5, verbose=True
+        ),
 
     }
 
