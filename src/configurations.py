efed76513d7a52539b3f3e352c936ef630dbbe35
HJacksons <jacksonherberts@gmail.com>
2024-04-15 01:41:29 +0200

    test hybrid false

--- src/configurations.py
+++ src/configurations.py
@@ -36,7 +36,7 @@ BATCH_SIZE = 64
 CLASSES = sorted(os.listdir(DATA))
 
 TRAINING = False
-AUGMENT = True
+AUGMENT = False
 DATATYPE = "potatodata"  # plantVillage or potatodata .
 
 NEW_DATASET = True # for the purpose of testing
